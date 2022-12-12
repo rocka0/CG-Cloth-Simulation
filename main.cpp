e194a7e3b005bf69791e75a67dc82bcfa8ed8a5d
Tushar Chenan <tusky2002@gmail.com>
2022-12-12 22:07:56 +0530

    Re-wrote the grid

--- main.cpp
+++ main.cpp
@@ -10,18 +10,16 @@
 
 using namespace std;
 
-constexpr int WIDTH = 1440;    // Width of the window
-constexpr int HEIGHT = 900;    // Height of the window
-
-constexpr int N = 19;                            // Number of points per row/col of cloth grid: [0, N]
+constexpr int WIDTH = 1440;                      // Width of the window
+constexpr int HEIGHT = 900;                      // Height of the window
+constexpr int N = 13;                            // Number of points per row/col of cloth mesh: [0, N]
 constexpr int pointCount = (N + 1) * (N + 1);    // Total number of points in the grid
-
-constexpr float fullsize = 6.0;    // intial zoom in on cloth
-constexpr float halfsize = fullsize / 2;
-
-constexpr float mass = 0.2f;                   // mass of each cloth point
-glm::vec3 gravity(0.0f, -0.00981f, 0.0f);      // gravity force vector in International System of Units
-constexpr float DEFAULT_DAMPING = -0.0125f;    // velocity damping of point
+constexpr float pointSpacing = 0.25f;            // Gap between two consecutive points of row/col in cloth mesh
+constexpr int fixedPointOne = N * (N + 1);
+constexpr int fixedPointTwo = pointCount - 1;
+constexpr float mass = 0.5f;                   // Mass of each cloth point
+glm::vec3 gravity(0.0f, -0.00981f, 0.0f);      // Gravity force vector in International System of Units
+constexpr float DEFAULT_DAMPING = -0.0125f;    // Velocity damping of point
 /*
     Ks = Spring constant in Hooke's law: F = (-Ks)(x)
     Kd = Spring damping constant: F = (-Kd)(v)
@@ -31,11 +29,12 @@ constexpr float KsStruct = 0.5f, KdStruct = -0.25f;
 constexpr float KsShear = 0.5f, KdShear = -0.25f;
 constexpr float KsBend = 0.85f, KdBend = -0.25f;
 
-constexpr int GRID_SIZE = 8;    // reference grid size
-
 constexpr float timeStep = 1 / 60.0f;    // dt time interval to update particle parameters
-double accumulator = timeStep;           // collects time intervals as frames are rendered
+double accumulator = 0.0f;               // Stores sum of time intervals until next dt time has elapsed
 LARGE_INTEGER frequency, t1, t2;         // t1, t2 store high accuracy time between 2 frames: t1 < t2
+
+constexpr int GRID_SIZE = 8;    // reference grid size
+constexpr float GRID_DEPTH = -3;
 
 bool showPoints = true;               // flag which marks if point of cloth are shown or not
 bool showStructuralSprings = true;    // flag to show structuralSprings
@@ -46,11 +45,11 @@ int mouseSelectedIndex = -1;          // if != -1, stores index of mouse selecte
 int prevX = 0;    // TODO
 int prevY = 0;    // TODO
 
-float rX = 45;    // stores the rotation angle along X for panning
-float rY = 0;     // stores the rotation angle along Y for panning
+float rX = 30;    // stores the rotation angle along X for rotating scene
+float rY = 0;     // stores the rotation angle along Y for rotating scene
 
-bool state = 1;      // tells us if we are zooming or panning
-float dist = -30;    // stores distance to scene
+bool state = 1;      // tells us if we are zooming or rotating
+float dist = -15;    // stores zoom distance
 
 GLint viewport[4];        // TODO
 GLdouble MV[16];          // TODO
@@ -66,6 +65,7 @@ struct Point {
     glm::vec3 pos;
     glm::vec3 velocity;
     glm::vec3 force;
+    bool isFixedPoint = false;
 };
 
 vector<Point> points;    // Array that stores all the points of the cloth
@@ -76,20 +76,18 @@ enum SPRING_TYPE { STRUCTURAL, SHEAR, BEND };    // Enum to store the various ty
     TODO
 */
 struct Spring {
-    int p1;
-    int p2;
+    Point &p1;
+    Point &p2;
     float restLength;
     float Ks;
     float Kd;
     SPRING_TYPE type;
 
-    Spring(int p1, int p2, float Ks, float Kd, SPRING_TYPE type) {
-        this->p1 = p1;
-        this->p2 = p2;
+    Spring(Point &p1, Point &p2, SPRING_TYPE type, float Ks, float Kd) : p1(p1), p2(p2) {
         this->Ks = Ks;
         this->Kd = Kd;
         this->type = type;
-        glm::vec3 diff = points[p1].pos - points[p2].pos;
+        glm::vec3 diff = p1.pos - p2.pos;
         this->restLength = sqrt(glm::dot(diff, diff));
     }
 };
@@ -104,51 +102,79 @@ void initGL() {
     QueryPerformanceCounter(&t1);
     glEnable(GL_DEPTH_TEST);
 
-    int n = N + 1;
+    auto rowMajor = [&](const int x, const int y) {
+        return (N + 1) * y + x;
+    };
 
-    points.resize(pointCount);
-    for (int j = 0, count = 0; j < n; j++) {
-        for (int i = 0; i < n; i++) {
-            Point &p = points[count++];
-            p.pos = glm::vec3(((float(i) / (n - 1)) * 2 - 1) * halfsize, fullsize + 1, ((float(j) / (n - 1)) * fullsize));
+    points.reserve(pointCount);
+    for (int j = 0; j <= N; j++) {
+        for (int i = 0; i <= N; i++) {
+            Point p = Point();
+            p.pos = glm::vec3(i * pointSpacing, j * pointSpacing, 0.0f);
             p.velocity = glm::vec3(0);
+            if (rowMajor(i, j) == fixedPointOne || rowMajor(i, j) == fixedPointTwo) {
+                p.isFixedPoint = true;
+            }
+            points.push_back(p);
         }
     }
 
-    // setup springs
-    // Horizontal
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < (n - 1); j++) {
-            springs.push_back(Spring((i * n) + j, (i * n) + j + 1, KsStruct, KdStruct, STRUCTURAL));
-        }
-
-    // Vertical
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < (n - 1); j++) {
-            springs.push_back(Spring((j * n) + i, ((j + 1) * n) + i, KsStruct, KdStruct, STRUCTURAL));
-        }
-
-    // Shearing Springs
-    for (int i = 0; i < (n - 1); i++) {
-        for (int j = 0; j < (n - 1); j++) {
-            springs.push_back(Spring((i * n) + j, ((i + 1) * n) + j + 1, KsShear, KdShear, SHEAR));
-            springs.push_back(Spring(((i + 1) * n) + j, (i * n) + j + 1, KsShear, KdShear, SHEAR));
+    for (int j = 0; j <= N; j++) {
+        for (int i = 0; i < N; i++) {
+            Point &p1 = points[rowMajor(i, j)];
+            Point &p2 = points[rowMajor(i + 1, j)];
+            springs.push_back(Spring(p1, p2, STRUCTURAL, KsStruct, KdStruct));
         }
     }
 
-    // Bend Springs
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < (n - 2); j++) {
-            springs.push_back(Spring((i * n) + j, (i * n) + j + 2, KsBend, KdBend, BEND));
+    for (int j = 0; j < N; j++) {
+        for (int i = 0; i <= N; i++) {
+            Point &p1 = points[rowMajor(i, j)];
+            Point &p2 = points[rowMajor(i, j + 1)];
+            springs.push_back(Spring(p1, p2, STRUCTURAL, KsStruct, KdStruct));
         }
-        springs.push_back(Spring((i * n) + (n - 3), (i * n) + (n - 1), KsBend, KdBend, BEND));
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < (n - 2); j++) {
-            springs.push_back(Spring((j * n) + i, ((j + 2) * n) + i, KsBend, KdBend, BEND));
+    for (int j = 0; j < N; j++) {
+        for (int i = 0; i < N; i++) {
+            Point &p1 = points[rowMajor(i, j)];
+            Point &p2 = points[rowMajor(i + 1, j + 1)];
+            springs.push_back(Spring(p1, p2, SHEAR, KsShear, KdShear));
         }
-        springs.push_back(Spring(((n - 3) * n) + i, ((n - 1) * n) + i, KsBend, KdBend, BEND));
+    }
+
+    for (int j = 0; j < N; j++) {
+        for (int i = 1; i <= N; i++) {
+            Point &p1 = points[rowMajor(i, j)];
+            Point &p2 = points[rowMajor(i - 1, j + 1)];
+            springs.push_back(Spring(p1, p2, SHEAR, KsShear, KdShear));
+        }
+    }
+
+    for (int j = 0; j <= N; j++) {
+        for (int i = 0; i + 2 <= N; i += 2) {
+            Point &p1 = points[rowMajor(i, j)];
+            Point &p2 = points[rowMajor(i + 2, j)];
+            springs.push_back(Spring(p1, p2, BEND, KsBend, KdBend));
+        }
+        for (int i = 1; i + 2 <= N; i += 2) {
+            Point &p1 = points[rowMajor(i, j)];
+            Point &p2 = points[rowMajor(i + 2, j)];
+            springs.push_back(Spring(p1, p2, BEND, KsBend, KdBend));
+        }
+    }
+
+    for (int i = 0; i <= N; i++) {
+        for (int j = 0; j + 2 <= N; j += 2) {
+            Point &p1 = points[rowMajor(i, j)];
+            Point &p2 = points[rowMajor(i, j + 2)];
+            springs.push_back(Spring(p1, p2, BEND, KsBend, KdBend));
+        }
+        for (int j = 1; j + 2 <= N; j += 2) {
+            Point &p1 = points[rowMajor(i, j)];
+            Point &p2 = points[rowMajor(i, j + 2)];
+            springs.push_back(Spring(p1, p2, BEND, KsBend, KdBend));
+        }
     }
 
     glPointSize(4);
@@ -160,9 +186,8 @@ void initGL() {
 */
 void drawScene() {
     QueryPerformanceCounter(&t2);
-    double frameTimeQP = (t2.QuadPart - t1.QuadPart) * 1000.0 / frequency.QuadPart;
+    accumulator += (t2.QuadPart - t1.QuadPart) * 1000.0 / frequency.QuadPart;
     t1 = t2;
-    accumulator += frameTimeQP;
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
@@ -180,10 +205,10 @@ void drawScene() {
     glBegin(GL_LINES);
     glColor3f(1.0f, 1.0f, 1.0f);
     for (int i = -GRID_SIZE; i <= GRID_SIZE; i++) {
-        glVertex3f((float) i, 0, (float) -GRID_SIZE);
-        glVertex3f((float) i, 0, (float) GRID_SIZE);
-        glVertex3f((float) -GRID_SIZE, 0, (float) i);
-        glVertex3f((float) GRID_SIZE, 0, (float) i);
+        glVertex3f((float) i, GRID_DEPTH, (float) -GRID_SIZE);
+        glVertex3f((float) i, GRID_DEPTH, (float) GRID_SIZE);
+        glVertex3f((float) -GRID_SIZE, GRID_DEPTH, (float) i);
+        glVertex3f((float) GRID_SIZE, GRID_DEPTH, (float) i);
     }
     glEnd();
 
@@ -205,22 +230,20 @@ void drawScene() {
     // draw springs
     glBegin(GL_LINES);
     for (auto &s : springs) {
-        auto &p1 = points[s.p1].pos;
-        auto &p2 = points[s.p2].pos;
         if (s.type == STRUCTURAL and showStructuralSprings) {
             glColor3f(0.0f, 1.0f, 0.0f);
-            glVertex3f(p1.x, p1.y, p1.z);
-            glVertex3f(p2.x, p2.y, p2.z);
+            glVertex3f(s.p1.pos.x, s.p1.pos.y, s.p1.pos.z);
+            glVertex3f(s.p2.pos.x, s.p2.pos.y, s.p2.pos.z);
         }
         if (s.type == SHEAR and showShearSprings) {
             glColor3f(248 / 255.0f, 249 / 255.0f, 136 / 255.0f);
-            glVertex3f(p1.x, p1.y, p1.z);
-            glVertex3f(p2.x, p2.y, p2.z);
+            glVertex3f(s.p1.pos.x, s.p1.pos.y, s.p1.pos.z);
+            glVertex3f(s.p2.pos.x, s.p2.pos.y, s.p2.pos.z);
         }
         if (s.type == BEND and showBendSprings) {
             glColor3f(107 / 255.0f, 102 / 255.0f, 157 / 255.0f);
-            glVertex3f(p1.x, p1.y, p1.z);
-            glVertex3f(p2.x, p2.y, p2.z);
+            glVertex3f(s.p1.pos.x, s.p1.pos.y, s.p1.pos.z);
+            glVertex3f(s.p2.pos.x, s.p2.pos.y, s.p2.pos.z);
         }
     }
     glEnd();
@@ -232,37 +255,37 @@ void drawScene() {
     TODO
 */
 void computeForces() {
-    for (int i = 0; i < pointCount; i++) {
-        auto &V = points[i].velocity;
-        auto &F = points[i].force;
+    for (auto &p : points) {
+        auto &V = p.velocity;
+        auto &F = p.force;
         F = glm::vec3(0);
 
         // add gravity force only for non-fixed points
-        if (i != 0 && i != N) F += gravity;
+        if (!p.isFixedPoint) F += gravity;
 
         // add force due to damping of velocity
         F += DEFAULT_DAMPING * V;
     }
 
     // add spring forces
-    for (size_t i = 0; i < springs.size(); i++) {
-        auto &P1 = points[springs[i].p1].pos;
-        auto &V1 = points[springs[i].p1].velocity;
-        auto &F1 = points[springs[i].p1].force;
-        auto &P2 = points[springs[i].p2].pos;
-        auto &V2 = points[springs[i].p2].velocity;
-        auto &F2 = points[springs[i].p2].force;
+    for (auto &s : springs) {
+        auto &P1 = s.p1.pos;
+        auto &V1 = s.p1.velocity;
+        auto &F1 = s.p1.force;
+        auto &P2 = s.p2.pos;
+        auto &V2 = s.p2.velocity;
+        auto &F2 = s.p2.force;
 
         glm::vec3 deltaP = P1 - P2;
         glm::vec3 deltaV = V1 - V2;
         float dist = glm::length(deltaP);
 
-        float leftTerm = -springs[i].Ks * (dist - springs[i].restLength);
-        float rightTerm = springs[i].Kd * (glm::dot(deltaV, deltaP) / dist);
+        float leftTerm = -s.Ks * (dist - s.restLength);
+        float rightTerm = s.Kd * (glm::dot(deltaV, deltaP) / dist);
         glm::vec3 springForce = (leftTerm + rightTerm) * glm::normalize(deltaP);
 
-        if (springs[i].p1 != 0 && springs[i].p1 != N) F1 += springForce;
-        if (springs[i].p2 != 0 && springs[i].p2 != N) F2 -= springForce;
+        if (!s.p1.isFixedPoint) F1 += springForce;
+        if (!s.p2.isFixedPoint) F2 -= springForce;
     }
 }
 
@@ -276,7 +299,6 @@ void eulerIntegrate() {
         auto &F = p.force;
         P += V * timeStep;
         V += (F * timeStep) / mass;
-        P.y = max(P.y, 0.0f);
     }
 }
 
@@ -284,21 +306,21 @@ void eulerIntegrate() {
     TODO
 */
 void ApplyProvotDynamicInverse() {
-    for (size_t i = 0; i < springs.size(); i++) {
-        auto &P1 = points[springs[i].p1].pos;
-        auto &V1 = points[springs[i].p1].velocity;
-        auto &P2 = points[springs[i].p2].pos;
-        auto &V2 = points[springs[i].p2].velocity;
+    for (auto &s : springs) {
+        auto &P1 = s.p1.pos;
+        auto &V1 = s.p1.velocity;
+        auto &P2 = s.p2.pos;
+        auto &V2 = s.p2.velocity;
         glm::vec3 deltaP = P1 - P2;
         float dist = glm::length(deltaP);
-        if (dist > springs[i].restLength) {
-            dist -= (springs[i].restLength);
+        if (dist > s.restLength) {
+            dist -= (s.restLength);
             dist /= 2.0f;
             deltaP = glm::normalize(deltaP);
             deltaP *= dist;
-            if (springs[i].p1 == 0 || springs[i].p1 == N) {
+            if (s.p1.isFixedPoint) {
                 V2 += deltaP;
-            } else if (springs[i].p2 == 0 || springs[i].p2 == N) {
+            } else if (s.p2.isFixedPoint) {
                 V1 -= deltaP;
             } else {
                 V1 -= deltaP;
